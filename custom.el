e2ecec8b0ab7502208e7ceb70ae9908d8d55ffbc
dytq <taariq.deda@gmail.com>
2021-02-02 20:37:02 +0100

    first commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("http://tumourrasmoinsbete.blogspot.com/feeds/posts/default" "http://tumourrasmoinsbete.blogspot.com/" "https://www.linuxtricks.fr/syndication/rss/news/0" "https://www.linuxtricks.fr/news/" "http://feeds.feedburner.com/TheHackersNews?format=xml")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
